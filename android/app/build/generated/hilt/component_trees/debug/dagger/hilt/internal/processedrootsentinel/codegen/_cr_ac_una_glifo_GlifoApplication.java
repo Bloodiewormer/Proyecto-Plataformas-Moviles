41c99e00037b7caccffcf8cf40dc70c86f6e6c41
Brandon Brenes U <brandon.brenes.umana@est.una.ac.cr>
2026-08-19 17:08:21 -0600

    chore: corregir .gitignore (excluir android/app/build y local.properties)

--- android/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_cr_ac_una_glifo_GlifoApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "cr.ac.una.glifo.GlifoApplication"
-)
-public final class _cr_ac_una_glifo_GlifoApplication {
-}
